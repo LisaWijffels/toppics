1712de20ecbbbbb540ab82fbdf7e4360bd0420e9
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-03-24 14:12:29 +0100

    added register.php

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Top Pics</title>
+</head>
+<body background="img/background.png">
+    <img src="img/logo2.png" alt="logo" id="logo">
+
+    <form action="/action_page.php" method="post" class="formlogin">
+
+        <label class="label" for="username">Username</label><br>
+        <input class="inputfield" type="text" name="username"><br>
+
+        <label class="label" for="email">Email</label><br>
+        <input class="inputfield" type="text" name="email"><br>
+
+        <label class="label" for="password">Password</label><br>
+        <input class="inputfield" type="text" name="password"><br>
+
+        <label class="label" for="repeatPassword">Repeat Password</label><br>
+        <input class="inputfield" type="text" name="repeatPassword"><br>
+
+        <input class="button" type="submit" value="Login">
+    </form>
+
+</body>
+</html>
