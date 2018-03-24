15511f5387b6446cba1263034bc1468e36af6df5
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-03-24 13:24:47 +0100

    added login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Top Pics</title>
+</head>
+<body>
+    <img src="img/logo-8.png" alt="logo" id="logo">
+
+    <form action="/action_page.php" method="post" class="formlogin">
+        <label class="label" for="username">Username</label><br>
+        <input class="inputfield" type="text" name="username"><br>
+        <label class="label" for="password">Password</label><br>
+        <input class="inputfield" type="text" name="password"><br>
+        <input class="button" type="submit" value="Login">
+    </form>
+
+    <a href="register.php" id="registerlink">Dont have an account yet? Sign up here.</a>
+
+</body>
+</html>
