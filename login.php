3970a04a86cc78235591d34475bf0773e452a629
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-03-24 14:07:21 +0100

    added css

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Top Pics</title>
 </head>
-<body background="background.png">
+<body background="img/background.png">
     <img src="img/logo2.png" alt="logo" id="logo">
 
     <form action="/action_page.php" method="post" class="formlogin">
