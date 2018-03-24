1e4d664c4ca8fac76fa4ff754ffa1ac20ba3d715
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-03-24 14:19:54 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Top Pics</title>
+</head>
+<body>
+
+    <nav>
+        <a href="index.php"><img src="img/logo2.png" alt="logo"></a>
+        <a href="index.php">Home</a>
+        <a href="#">Profile</a>
+        <a href="#">Discover</a>
+        <a href="#">Friends</a>
+    </nav>
+
+</body>
+</html>
